6dc560b2a7b300d48c2e4e874e7df2b3fa80ae48
berezoviy <berezoviy.vasil@gmail.com>
2015-05-06 14:17:25 +0300

    Added basic components

---
+++ public/scripts/commentbox.js
@@ -0,0 +1,36 @@
+var CommentList = React.createClass({
+  render: function{
+    return(
+        <div className="commentList">
+          Heeelloo. Comment List.
+        </div>
+      );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+        <div className="commentForm">
+          Comment Form.
+        </div>
+      )
+  }
+})
+
+var CommentBox = React.createClass({
+  render: function(){
+    return (
+        <div className="commentBox">
+          Hello, retards! I am crazy Comment Box.
+          <h1>Comments</h1>
+          <CommentList />
+          <CommentForm />
+        </div>
+      );
+  }
+});
+React.render(
+  <CommentBox />,
+  document.getElementById('content');
+  );
