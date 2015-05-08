1a76bdb405f91d0a971ca545e3d3990fcadaad91
berezoviy <berezoviy.vasil@gmail.com>
2015-05-08 16:45:34 +0300

    Change text

--- public/scripts/commentbox.js
+++ public/scripts/commentbox.js
@@ -94,7 +94,7 @@ var CommentBox = React.createClass({
   render: function(){
     return (
         <div className="commentBox">
-          Hello, retards! I am crazy Comment Box.
+          Hello! I am Comment Box.
           <h1>Comments</h1>
           <CommentList data={this.state.data} />
            <CommentForm onCommentSubmit={this.handleCommentSubmit} />
